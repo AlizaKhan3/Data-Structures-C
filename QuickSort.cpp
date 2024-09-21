3ba9cafc86f8cc7bad9b5604c4a1596bec95ffcd
Aliza Khan <124785215+AlizaKhan3@users.noreply.github.com>
2024-09-21 22:00:39 +0500

    Create QuickSort.cpp

---
+++ QuickSort.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int partition(int arr[], int lowerBound, int upperBound) {
+    int pivotElement = arr[lowerBound];
+    int start = lowerBound;
+    int end = upperBound;
+
+    while (start < end) {
+        while (arr[start] <= pivotElement) {
+            start++;
+        }
+        while (arr[end] > pivotElement) {
+            end--;
+        }
+        if (start < end) {
+            swap(arr[start], arr[end]);
+        }
+    }
+    swap(arr[lowerBound], arr[end]);
+    return end;
+}
+
+void QuickSort(int arr[], int lowerBound, int upperBound) {
+    if (lowerBound < upperBound) {
+        int location = partition(arr, lowerBound, upperBound);
+        QuickSort(arr, lowerBound, location - 1);
+        QuickSort(arr, location + 1, upperBound);
+    }
+}
+
+int main() {
+    int arr[] = {7, 6, 10, 5, 9, 2, 1, 15, 7};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    QuickSort(arr, 0, size - 1);
+
+    std::cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
