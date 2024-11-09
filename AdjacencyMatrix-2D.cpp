20de3445fde71b3a221094d3781232bef53be99d
Aliza Khan <124785215+AlizaKhan3@users.noreply.github.com>
2024-11-09 19:30:16 +0500

    Create AdjacencyMatrix-2D.cpp

---
+++ AdjacencyMatrix-2D.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+#include <>
+void printGraph(const vector<vector<int>>& graph, int V){
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            cout << graph[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int V; // Number of vertices
+    cout<<"Enter verticies";
+    cin>>V;
+    vector<vector<int>> graph(V, vector<int>(V));
+    
+    cout<<"Enter Array Elements: ";
+    for(int i =0; i<V; i++){
+      for(int j =0; j<V; j++){
+        cin>>graph[i][j];
+      }
+      cout<<endl;
+    }
+    
+    printGraph(graph, V);
+    return 0;
+}
